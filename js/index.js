cf457ba139ea67f5aedef7dd691260bedbd9cc37
milanesadeternera <facundojcolombo@gmail.com>
2024-09-24 14:53:24 -0300

    index modal

--- js/index.js
+++ js/index.js
@@ -1,3 +1,24 @@
+
 document.addEventListener("DOMContentLoaded", () =>{
+    console.log("falopa");
+    //login
+    document.getElementById("loginButton").addEventListener("click", login);
+    //registro
+    document.getElementById("registerButton").addEventListener("click", register);
     //Recupero lista de discos.
 })
+
+
+function register(){
+    console.log("entro a register");
+    let username = document.getElementById("registerUser");
+    let email = document.getElementById("registerEmail");
+    console.log("Usuario:"+username.value);
+    console.log("Email:"+email.value);
+    if(username.value == '' ){
+        username.classList.add("is-invalid");
+    }
+
+}
+
+function login(){}
